a94cf70bbf90389bf225f983f50d9a1e25fa02a4
Tomo <git.doorbell495@silomails.com>
2024-04-18 17:40:53 +0200

    updated pkgs.sh

--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -7,10 +7,20 @@ DIR="$HOME/.backup-packages"
 BACKUP_FILE="BackupPackages-$(date +%d-%m-%y).txt"
 BACKUP_FILE_AUR="BackupPackages-AUR-$(date +%d-%m-%y).txt"
 
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus &&
+
 if [ -d "$DIR" ]; then
     rm -f "$DIR"/*
 fi
 mkdir -p "$DIR"
 pacman -Qqen > "$DIR/$BACKUP_FILE"
 pacman -Qqem > "$DIR/$BACKUP_FILE_AUR"
+
+# Finds the words and deletes them without spaces 
+for word in base base -devel bash linux -lts-headers -lts -headers -firmware vim
+do
+  sed -i "s/${word}//g; /^$/ d" $DIR/$BACKUP_FILE
+done
+
+sed -i "s/paru-bin-debug//g; /^$/ d" $DIR/$BACKUP_FILE_AUR
 echo "Installed packages backed up to '$DIR'"
