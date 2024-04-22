b2784ff350aeb928c2c4f2674e4e7bf47214c45a
Tomo <git.doorbell495@silomails.com>
2024-04-22 20:17:06 +0200

    fixed pkgs.sh

--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -3,24 +3,24 @@
 # This script backs up installed packages on Arch Linux.
 # to reinstall all packages use : (pacman|paru) -S - < pathtoyourpackagelist.txt
 
-DIR="$HOME/.backup-packages"
-BACKUP_FILE="BackupPackages-$(date +%d-%m-%y).txt"
-BACKUP_FILE_AUR="BackupPackages-AUR-$(date +%d-%m-%y).txt"
+dir="$HOME/.backup-packages"
+backup_file="BackupPackages-$(date +%d-%m-%y).txt"
+backup_file_aur="BackupPackages-AUR-$(date +%d-%m-%y).txt"
+no_pkgs="base base -devel bash linux-lts-headers linux-lts linux-headers linux-firmware ttf-linux-li    bertine linux vim"
 
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus &&
 
-if [ -d "$DIR" ]; then
-    rm -f "$DIR"/*
+if [ -d "$dir" ]; then
+    rm -f "$dir"/*
 fi
-mkdir -p "$DIR"
-pacman -Qqen > "$DIR/$BACKUP_FILE"
-pacman -Qqem > "$DIR/$BACKUP_FILE_AUR"
+mkdir -p "$dir"
+pacman -Qqen > "$dir/$backup_file"
+pacman -Qqem > "$dir/$backup_file_aur"
 
 # Finds the words and deletes them without spaces 
-for word in base base -devel bash linux -lts-headers -lts -headers -firmware -libertine ttf- vim
-do
-  sed -i "s/${word}//g; /^$/ d" $DIR/$BACKUP_FILE
+for word in $no_pkgs; do
+  sed -i "s/${word}//g; /^$/ d" $dir/$backup_file
 done
 
-sed -i "s/paru-bin-debug//g; /^$/ d" $DIR/$BACKUP_FILE_AUR
-echo "Installed packages backed up to '$DIR'"
+sed -i "s/paru-bin-debug//g; /^$/ d" $dir/$backup_file_aur
+echo "Installed packages backed up to '$dir'"
