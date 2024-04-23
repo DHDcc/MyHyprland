c03da24473726138eba6b9f3e9bd444404158a13
Tomo <git.doorbell495@silomails.com>
2024-04-23 21:57:29 +0200

    updated wallpaper-randomize.sh

--- scripts/wallpaper-randomize.sh
+++ scripts/wallpaper-randomize.sh
@@ -14,34 +14,13 @@ fi
 
 RANDOMPICS="${PICS[$((RANDOM % ${#PICS[@]}))]}"
 
-change_swww() {
-  echo "Running change_swww function..."
-  swww query || swww init
-  if [ -f "${RANDOMPICS}" ]; then
+echo "Running change_swww function..."
+swww query || swww init
+if [ -f "${RANDOMPICS}" ]; then
     echo "Setting wallpaper to ${RANDOMPICS}"
     notify-send -t 1710 -i ~/icons/wallpapers.icon.png --hint int:transient:1 "Changing wallpaper...."
     swww img "${RANDOMPICS}" --transition-fps 170 --transition-type any --transition-duration 3
-  else
-    echo "Error: Image file not found: ${RANDOMPICS}" >&2
-    exit 1
-  fi
-}
-
-change_current() {
-  echo "Running change_current function..."
-  change_swww
-}
-
-echo "Starting script..."
-case "$1" in
-  "swww")
-    change_swww
-    ;;
-  "s")
-    change_current
-    ;;
-  *)
-	  echo "Error: Try 'swww' or 's' at the end of : $0 " >&2
-    exit 1
-    ;;
-esac
+else
+   echo "Error: Image file not found: ${RANDOMPICS}" >&2
+   exit 1
+fi
