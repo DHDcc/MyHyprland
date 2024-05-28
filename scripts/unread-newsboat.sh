85a4ab772bc16640456c3ba3faf3a2651cd886e4
Tomo <git.doorbell495@silomails.com>
2024-05-28 15:32:07 +0200

    fix (unread-newsboat.sh): manualy read from the rcfile instead of echoing the value of 0 to stop notification loop

--- scripts/unread-newsboat.sh
+++ scripts/unread-newsboat.sh
@@ -12,15 +12,15 @@ icon=$HOME/icons/newspaper.png
 if ! grep -q "^export NEWSBOAT_LAST_NUM=" "$rcfile"; then
      echo "export NEWSBOAT_LAST_NUM=0" >> "$rcfile"
 fi
-last_num=$(echo "$NEWSBOAT_LAST_NUM")
+last_num=$(grep "^export NEWSBOAT_LAST_NUM=" "$rcfile" | cut -d '=' -f 2)
 
 print_unread=$(command newsboat -x reload print-unread || exit 1)
 num=$(echo "$print_unread" | awk '{print $1}')
 
-if [ "$num" -eq "$last_num" ]; then
+if [[ $num -eq $last_num ]]; then
      exit 0
-elif [ "$num" -ne 0 ]; then
-	notify-send -i "$icon" -u normal -t 4000 "Newsboat" "You have $num new articles"
+elif [[ $num -ne 0 ]]; then
+    notify-send -i "$icon" -u normal -t 4000 "Newsboat" "You have $num new articles"
     sed -i "s|^export NEWSBOAT_LAST_NUM=.*|export NEWSBOAT_LAST_NUM=$num|" "$rcfile"
     exec zsh
 else
