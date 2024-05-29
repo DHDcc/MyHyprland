ee9e1d9a6f064fab5c1456ac77b635c622fdb745
Tomo <git.doorbell495@silomails.com>
2024-05-29 21:04:43 +0200

    change (unread-newsboat.sh): re-using a .file instead of using variables

--- scripts/unread-newsboat.sh
+++ scripts/unread-newsboat.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # This script will send a notification if there are any new articles.
 # Add "alias newsboat='~/YOUR_SCRIPTS_DIR/newsboat'" in your zshrc (you need the newsboat script)
@@ -6,23 +6,21 @@
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus 
 # -------------------------------------------------------------------
 
-rcfile=$HOME/.zshrc
-icon=$HOME/icons/newspaper.png
+file=$HOME/.config/newsboat/.last-num
+icon=$HOME/icons/newsboat.svg
 
-if ! grep -q "^export NEWSBOAT_LAST_NUM=" "$rcfile"; then
-     echo "export NEWSBOAT_LAST_NUM=0" >> "$rcfile"
+if [ ! -f "$file" ]; then
+     echo 0 > "$file"
 fi
-last_num=$(grep "^export NEWSBOAT_LAST_NUM=" "$rcfile" | cut -d '=' -f 2)
 
-print_unread=$(command newsboat -x reload print-unread || exit 1)
-num=$(echo "$print_unread" | awk '{print $1}')
+num=$(command newsboat -x reload print-unread | awk '{print $1}')
+last_num=$(cat $file)
 
-if [[ $num -eq $last_num ]]; then
-     exit 0
-elif [[ $num -ne 0 ]]; then
-    notify-send -i "$icon" -u normal -t 4000 "Newsboat" "You have $num new articles"
-    sed -i "s|^export NEWSBOAT_LAST_NUM=.*|export NEWSBOAT_LAST_NUM=$num|" "$rcfile"
-    exec zsh
+if [[ $num == $last_num ]]; then
+    exit 0
+elif [[ "$num" -ne 0 ]]; then
+    notify-send -i "$icon" -u normal -t 4000 "Newsboat" "You have "$num" new articles"
+    echo "$num" > "$file"
 else
     exit 0
 fi
