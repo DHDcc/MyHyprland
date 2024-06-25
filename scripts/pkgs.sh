5f47fb68591b001af18119271a43ebc6d3f5eabe
Tomo <git.doorbell495@silomails.com>
2024-06-26 01:03:38 +0200

    change: improved pkg.sh

--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -1,26 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script backs up installed packages on Arch Linux.
 # to reinstall all packages use : (pacman|paru) -S - < pathtoyourpackagelist.txt
-
+## NEED TO FIX !!!
 dir="$HOME/.backup-packages"
-backup_file="BackupPackages-$(date +%d-%m-%y).txt"
-backup_file_aur="BackupPackages-AUR-$(date +%d-%m-%y).txt"
-no_pkgs="base base -devel bash linux-lts-headers linux-lts linux-headers linux-firmware ttf-linux-li    bertine linux vim"
+backup_file="$dir/BackupPackages-$(date +%d-%m-%y).txt"
+backup_file_aur="$dir/BackupPackages-AUR-$(date +%d-%m-%y).txt"
+exclude_pkgs=(
+	      bash 
+	      linux-lts 
+	      vim base 
+	      base-devel 
+	      linux 
+	      linux-firmware  
+	      linux-headers 
+	      linux-lts-headers 
+)
 
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus &&
+pattern=$(printf "|%s" "${exclude_pkgs[@]}")
+pattern=${pattern:1}  # Remove the leading '|'
+current_pkgs=$(pacman -Qqen | grep -v -E "^(${pattern})$")
+current_pkgs_aur=$(pacman -Qqem)
 
-if [ -d "$dir" ]; then
-    rm -f "$dir"/*
+if [ ! -d "$dir" ]; then
+    mkdir -p "$dir"
 fi
-mkdir -p "$dir"
-pacman -Qqen > "$dir/$backup_file"
-pacman -Qqem > "$dir/$backup_file_aur"
 
-# Finds the words and deletes them without spaces 
-for word in $no_pkgs; do
-  sed -i "s/${word}//g; /^$/ d" $dir/$backup_file
-done
+echo "$current_pkgs" > "$backup_file"
+echo "$current_pkgs_aur" > "$backup_file_aur"
 
-sed -i "s/paru-bin-debug//g; /^$/ d" $dir/$backup_file_aur
-echo "Installed packages backed up to '$dir'"
+#echo "Installed packages backed up to '$dir'"
