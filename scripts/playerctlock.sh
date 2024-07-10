ba9ba40236b9b44852a8e9cbd537fd8e1614eafe
Tomo <git.doorbell495@silomails.com>
2024-07-10 11:34:09 +0200

    change: optimized playerctlock.sh

--- scripts/playerctlock.sh
+++ scripts/playerctlock.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
+# Check for arguments
 if [ $# -eq 0 ]; then
 	echo "Usage: $0 --title | --arturl | --artist | --length | --album | --source"
 	exit 1
 fi
 
 # Function to get metadata using playerctl
-get_metadata() {
+getMetadata() {
 	key=$1
 	playerctl metadata --format "{{ $key }}" 2>/dev/null
 }
 
-# Check for arguments
-
 # Function to determine the source and return an icon and text
-get_source_info() {
-	trackid=$(get_metadata "mpris:trackid")
+getSourceInfo() {
+	trackid=$(getMetadata "mpris:trackid")
 
 	case "$trackid" in
 		*Feishin* ) echo -e "Feishin " ;;
@@ -25,7 +24,7 @@ get_source_info() {
 	esac
 }
 
-get_cover() {
+getCover() {
        local tempfile="/tmp/tmp.xG2g4TRv4i"
        local path="/tmp/cover.png"
 
@@ -40,46 +39,44 @@ get_cover() {
 # Parse the argument
 case "$1" in
 --title)
-	title=$(get_metadata "xesam:title")
+	title=$(getMetadata "xesam:title")
 
-	if [ ! -z "$title" ]; then
+	if [ -n "$title" ]; then
 		echo "${title:0:28}" # Limit the output to 50 characters
 	else
-		: # Do nothing
+		exit 1 
 	fi
 	;;
 --arturl)
-	url=$(get_metadata "mpris:artUrl")
+	url=$(getMetadata "mpris:artUrl")
 
-	[[ -z "$url" ]] && : # Do nothing
+	[[ -z "$url" ]] && exit 1
 	if [[ "$url" == file://* ]]; then
 		url=${url#file://}
-	elif [[ "$url" == http://192.168.1.20:8096/* ]]; then
-		get_cover
+	elif [[ "$url" == http://* ]] || [[ "$url" == https://* ]]; then
+		getCover
 	fi
 	echo "$url"
 	;;
 --artist)
-	artist=$(get_metadata "xesam:artist")
+	artist=$(getMetadata "xesam:artist")
 
-	if [ ! -z "$artist" ]; then
+	if [ -n "$artist" ]; then
 		echo "${artist:0:30}" # Limit the output to 50 characters
 	else
-		: # Do nothing
+		exit 1
 	fi
 	;;
 --length)
-	duration=$(get_metadata "mpris:length")
-	duration_in_seconds=$((duration / 1000000))
-	remaining_time=$((duration_in_seconds - current_position))
-	minutes=$((remaining_time / 60))
-	seconds=$((remaining_time % 60))
+	duration=$(getMetadata "mpris:length")
 
-	if [ ! -z "$duration" ]; then
-		printf "%02d:%02d" "$minutes" "$seconds"
-	else
-		: # Do nothing
-	fi
+	[ -z "$duration" ] && exit 1
+
+	durationInSeconds=$((duration / 1000000))
+	minutes=$((durationInSeconds / 60))
+	seconds=$((durationInSeconds % 60))
+
+        printf "%02d:%02d" "$minutes" "$seconds"
 	;;
 --status)
 	status=$(playerctl status 2>/dev/null)
@@ -89,11 +86,11 @@ case "$1" in
 	elif [[ $status == "Playing" ]]; then
 		echo ""
 	else
-		: # Do nothing
+		exit 1 
 	fi
 	;;
 --album)
-	album=$(playerctl metadata --format "{{ xesam:album }}" 2>/dev/null)
+	album=$(getMetadata "xesam:album")
 	length=${#album}
 	lim="20"
 
@@ -105,11 +102,11 @@ case "$1" in
 		fi
 
 	else
-	      : # Do nothing
+	       exit 1
 	fi
 	;;
 --source)
-	get_source_info
+	getSourceInfo
 	;;
 *)
 	echo "Invalid option: $1"
