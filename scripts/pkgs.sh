82c0ef396d65483fb56cbe849a0b056e040458eb
Tomo <git.doorbell495@silomails.com>
2024-07-12 05:33:22 +0200

    fix: will properly remove files in the directory

--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
-# This script backs up installed packages on Arch Linux.
-# to reinstall all packages use : (pacman|paru) -S - < pathtoyourpackagelist.txt
 
-dir="$HOME/.backup-packages"
-backup_file="$dir/BackupPackages-$(date +%d-%m-%y).txt"
-backup_file_aur="$dir/BackupPackages-AUR-$(date +%d-%m-%y).txt"
-exclude_pkgs=(
-	      bash 
-	      linux-lts 
-	      vim base 
-	      base-devel 
-	      linux 
-	      linux-firmware  
-	      linux-headers 
-	      linux-lts-headers 
+# This script backs up installed packages on Arch Linux.
+# to reinstall all packages use: {pacman | paru} -S - < PathToYourPackageList.txt
+
+dir="/home/user/.backup-packages"
+files=($dir/*)
+backupFile="$dir/BackupPackages-$(date +%d-%m-%y).txt"
+backupFileAur="$dir/BackupPackagesAur-$(date +%d-%m-%y).txt"
+excludePkgs=(
+	      bash \ 
+	      linux-lts \ 
+	      vim base \
+	      base-devel \ 
+	      linux \
+	      linux-firmware \ 
+	      linux-headers \
+	      linux-lts-headers \
 )
 
-pattern=$(printf "|%s" "${exclude_pkgs[@]}")
+pattern=$(printf "|%s" "${excludePkgs[@]}")
 pattern=${pattern:1}  # Remove the leading '|'
-current_pkgs=$(pacman -Qqen | grep -v -E "^(${pattern})$")
-current_pkgs_aur=$(pacman -Qqem)
+currentPkgs=$(pacman -Qqen | grep -v -E "^(${pattern})$")
+currentPkgsAur=$(pacman -Qqem)
 
-if [ ! -d "$dir" ]; then
-    mkdir -p "$dir"
-fi
+[ ! -d "$dir" ] && mkdir -p "$dir"
+[ ${#files[@]} -gt 0 ] && rm ${dir}/*       
 
-echo "$current_pkgs" > "$backup_file"
-echo "$current_pkgs_aur" > "$backup_file_aur"
-
-#echo "Installed packages backed up to '$dir'"
+echo "$currentPkgs" > "$backupFile"
+echo "$currentPkgsAur" > "$backupFileAur"
