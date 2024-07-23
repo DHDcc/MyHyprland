3323be56b4fd657acfc8e8b182b7d32f2f5cc05c
Tomo <github.gas371@slmail.me>
2024-07-24 00:07:49 +0200

    change: changed variables names

--- .scripts/pkgs.sh
+++ .scripts/pkgs.sh
@@ -3,28 +3,29 @@
 # This script backs up installed packages on Arch Linux.
 # to reinstall all packages use: {pacman | paru} -S - < PathToYourPackageList.txt
 
-dir="/home/user/.backup-packages"
-files=($dir/*)
-backupFile="$dir/BackupPackages-$(date +%d-%m-%y).txt"
-backupFileAur="$dir/BackupPackagesAur-$(date +%d-%m-%y).txt"
-excludePkgs=(
-	      bash \ 
-	      linux-lts \ 
-	      vim base \
-	      base-devel \ 
-	      linux \
-	      linux-firmware \ 
-	      linux-headers \
-	      linux-lts-headers \
+backupPackagesDir="/home/user/.backup-packages"
+backupPackagesfiles=("${backupPackagesDir}"/*)
+backupFilePacman="${backupPackagesDir}/BackupPackagesPacman-$(date +%d-%m-%y).txt"
+backupFileAur="${backupPackagesDir}/BackupPackagesAur-$(date +%d-%m-%y).txt"
+excludedPackages=(
+	      "bash"
+	      "zsh"
+	      "linux-lts" 
+	      "vim"
+	      "base" 
+	      "base-devel"  
+	      "linux" 
+	      "linux-firmware"
+	      "linux-headers"
+	      "linux-lts-headers"
 )
+excludedPackages=$(printf "|%s" "${excludedPackages[@]}")
+excludedPackages=${excludedPackages:1}  # Remove the leading '|'
+currentPkgsPacman=$(pacman -Qqen | grep -v -E "^(${excludedPackages})$")
+currentPkgsAur=$(pacman -Qqem | grep -v -E "^(${excludedPackages})$")
 
-pattern=$(printf "|%s" "${excludePkgs[@]}")
-pattern=${pattern:1}  # Remove the leading '|'
-currentPkgs=$(pacman -Qqen | grep -v -E "^(${pattern})$")
-currentPkgsAur=$(pacman -Qqem)
+[[ ! -d "${backupPackagesDir}" ]] && mkdir -p "${backupPackagesDir}"
+[[ "${#backupPackagesfiles[@]}" -gt 0 ]] && rm -f "${backupPackagesDir}"/*  # Check if backupPackages directory contains files
 
-[ ! -d "$dir" ] && mkdir -p "$dir"
-[ ${#files[@]} -gt 0 ] && rm ${dir}/*       
-
-echo "$currentPkgs" > "$backupFile"
-echo "$currentPkgsAur" > "$backupFileAur"
+echo "${currentPkgsPacman}" > "${backupFilePacman}"
+echo "${currentPkgsAur}" > "${backupFileAur}"
