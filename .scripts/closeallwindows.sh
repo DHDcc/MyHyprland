ad08f1ba6bac81ef56d24d926ee38a7364bb0d5f
Tomo <github.gas371@slmail.me>
2024-10-15 00:02:55 +0200

    refactor: update hyprland config

--- .scripts/closeallwindows.sh
+++ .scripts/closeallwindows.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 windowNames=$(hyprctl clients | grep "class:" | awk '{print $2}')
-windowNames=$(printf '%s' "${windowsNames,,}")
+windowNames=$(printf '%s' "${windowNames,,}")
 
 # Convert the window names into an array
 IFS=$'\n' read -r -d '' -a windowsArray <<< "$windowNames"
 
 for windowName in "${windowsArray[@]}"; do
-      hyprctl dispatch closewindow "${windowName}"
+      hyprctl dispatch closewindow  "${windowName}"
 done
